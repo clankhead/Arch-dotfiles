baeb23452efa867f6300f9a6a934d0f885406c5d
clankhead <76953624+clankhead@users.noreply.github.com>
2026-05-15 04:56:53 -0400

    Add files via upload

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,46 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+
+    # Basic aliases
+    alias ls='ls --color=auto'
+    alias ll='ls -alF'
+    alias la='ls -A'
+    alias l='ls -CF'
+    alias grep='grep --color=auto'
+
+    # Custom aliases
+    alias py='python3'
+    alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+    alias plz='sudo'
+    alias please='sudo'
+    alias v='nvim'
+    alias vim='nvim'
+
+    # Set wallpaper function
+    function wp
+        feh --bg-scale $argv[1]
+    end
+
+    # PATH
+    fish_add_path $HOME/.local/bin
+
+    # Starship
+    set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
+    starship init fish | source
+
+    # GTK theme
+    set -gx GTK_THEME Dracula
+
+    # Key Bindings
+    bind \es 'fish_commandline_prepend sudo'
+
+    # yazi shell wrapper (cd on exit)
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        command yazi $argv --cwd-file="$tmp"
+        if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+            builtin cd -- "$cwd"
+        end
+        command rm -f -- "$tmp"
+    end
+
 end
